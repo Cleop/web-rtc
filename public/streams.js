fdb815ae2125970e7a7531af7329424a4a7a4ccd
Cleop <cleo.pearson@hotmail.co.uk>
2017-01-23 12:22:37 +0000

    Creates index.html file and streams.js

---
+++ public/streams.js
@@ -0,0 +1,14 @@
+// var promise = navigator.mediaDevices.getUserMedia({
+// video: true,
+// audio: true
+// });
+// promise.then((avSteam) => {
+// // Find my video tag…
+// video = document.createElement('video');
+// attachMediaStream(video, stream);
+// video.play();
+// // Add video tag to DOM
+// videoContainer.append(v);
+// }).catch(() => {…});
+
+console.log("It worked!")
